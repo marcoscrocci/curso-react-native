fc6a33e894b7c004ca7baa1c7d3344add77a8705
marcoscrocci@gmail.com <marcoscrocci@gmail.com>
2019-12-01 17:56:27 -0300

    Final das aulas dos pré requisitos

--- exercicios-express/index - Copia.js
+++ exercicios-express/index - Copia.js
@@ -22,14 +22,8 @@ app.use((req, res, next) => {
     next()
 })
 
-// http://localhost:3000/clientes/relatorio?completo=true&ano=2018
 app.get('/clientes/relatorio', (req, res) => {
     res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
-})
-
-// http://localhost:3000/clientes/5
-app.get('/clientes/:id', (req, res) => {
-    res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
 app.post('/corpo', (req, res) => {
@@ -44,37 +38,11 @@ app.post('/corpo', (req, res) => {
     res.send(req.body)
 })
 
-//app.use((req, res) => { // Para atender qualquer método (get/post/put/delete)
-app.get('/', (req, res) => { // Atende apenas o método GET
-    res.send('Método GET')
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
-app.post('/', (req, res) => { // Atende apenas o método POST
-    res.send('Método POST')
-})
-
-app.put('/', (req, res) => { // Atende apenas o método PUT
-    res.send('Método PUT')
-})
-
-app.delete('/', (req, res) => { // Atende apenas o método DELETE
-    res.send('Método DELETE')
-})
-
-app.get('/json', (req, res) => { // Atende apenas o método GET
-    res.json({
-        nome: 'iPad 32GB',
-        price: 1899.00,
-        discount: 0.12
-    })
-})
-
-app.use('/jsonarray', (req, res, next) => {
-    console.log('Antes...')
-    next()
-})
-
-app.get('/jsonarray', (req, res, next) => { // Atende apenas o método GET
+app.get('/opa', (req, res, next) => {
     console.log('Durante...')
     res.json({
         data: [
@@ -87,17 +55,21 @@ app.get('/jsonarray', (req, res, next) => { // Atende apenas o método GET
         limit: 3,
         status: 200
     })
+
     next()
+    
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
+    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
 })
 
-app.use('/jsonarray', (req, res, next) => {
+app.use((req, res) => {
     console.log('Depois...')
-    next()
 })
-
-/*app.use('/teste', (req, res) => {
-    res.send('Estou testando!')
-})*/
 
 app.listen(3000, () => {
     console.log('Backend executando...')
