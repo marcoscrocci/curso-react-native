1aee8618e81eb8c0bd9ad682769d9c90e7f1872d
marcoscrocci@gmail.com <marcoscrocci@gmail.com>
2020-01-22 20:34:58 -0300

    22/01/2020 20h34

--- mines/App.js
+++ mines/App.js
@@ -28,6 +28,11 @@ export default class App extends Component {
         <Text style={styles.sectionDescription}>Tamanho da grade:
             {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
         <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
       </View>
 
     )
